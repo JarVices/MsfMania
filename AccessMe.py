eda0001273cd88f501efe9edf455acce539eed84
G1ft3dC0d3 <52042546+G1ft3dC0d3@users.noreply.github.com>
2019-07-13 15:19:05 +0200

    Add files via upload

--- AccessMe.py
+++ AccessMe.py
@@ -1,1 +1,13 @@
+from lib import core
+
+core.banner()
+
+def main():
+    core.choice()
+
+    Fwhile = True
+    while Fwhile:
+
+
+
 
--- lib/core.py
+++ lib/core.py
@@ -1,1 +1,31 @@
+import os
+
+def banner():
+
+    print("""
+    
+    
+                     Payload Generator Script 
+        
+                        Named : 'AccessMe'
+            
+        This program will be used as part of a security audit
+    
+                          Version : 0.1
+                          
+                          
+    """)
+
+
+def clear():
+    os.system("clear")
+
+
+
+def choice():
+    print("""
+    
+    [1] Advanced Windows Meterpreter 
+    
+    """)
 
