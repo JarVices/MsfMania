1d2cbd454d76bb9c413dc58b1bb6d2e096784548
G1ft3dC0d3 <52042546+G1ft3dC0d3@users.noreply.github.com>
2019-08-18 04:11:18 +0200

    Add files via upload

--- template.c
+++ template.c
@@ -1,7 +1,10 @@
 #include <windows.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char **argv) {
 	char shellcode[] = {
+
 
 };
 
@@ -10,4 +13,5 @@ int main(int argc, char **argv) {
 	void *exec = VirtualAlloc(0, sizeof shellcode, MEM_COMMIT, PAGE_EXECUTE_READWRITE);
 	memcpy(exec, shellcode, sizeof shellcode);
 	((void(*)())exec)();
+
 }
