c8ae6fd4fbc8cbc34e41c3a2cf19ddf662a465aa
G1ft3dC0d3 <52042546+G1ft3dC0d3@users.noreply.github.com>
2019-08-26 04:47:38 +0200

    Add files via upload

--- Other/test.py
+++ Other/test.py
@@ -1,2 +1,11 @@
-test = "filename" + ".exe"
-print(test)
+import subprocess
+
+
+ICON = "test.ico"
+RC = 'id ICON "/root/AccessMe/icon/'
+RC += ''.join((ICON, '"'))
+with open('/root/AccessMe/icon/AccessMe.rc', 'w') as f:
+    f.write(RC)
+
+WINDRES = ['windres', '/root/AccessMe/icon/AccessMe.rc', '-O', 'coff', '-o', '/root/AccessMe/icon/AccessMe.res']
+subprocess.run(WINDRES, shell=False, stdout=subprocess.PIPE).stdout.decode('utf-8')
