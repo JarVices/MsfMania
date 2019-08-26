e2fcd459c79302bfd9b2ea3c8f4071ffb1b20c00
G1ft3dC0d3 <52042546+G1ft3dC0d3@users.noreply.github.com>
2019-08-26 04:45:59 +0200

    Add files via upload

--- AccessMe.py
+++ AccessMe.py
@@ -1,119 +1,168 @@
 #Import
+import os
+
 from lib import core
 from payload.x86_Stager.Windows import x86_Win_Meterpreter_Rev_Http, x86_Win_Meterpreter_Rev_Https, x86_Win_Meterpreter_Rev_Tcp
 from payload.x64_Stager.Windows import x64_Win_Meterpreter_Rev_Http, x64_Win_Meterpreter_Rev_Https, x64_Win_Meterpreter_Rev_Tcp
 
 
-#Displays the main banner
-core.Banner()
+#def Check_Environnement():
+#ln -s /usr/bin/x86_64-w64-mingw32-gcc /usr/bin/windres
+#ln -s /usr/bin/i686-w64-mingw32-windres /usr/bin/windres_1
+
 
 
 #Main function
 def Modules():
 
-    core.Module_Choice()
+    try:
 
-    Modules_while = True
+        core.Clear()
+        core.Banner()
+        core.Module_Choice()
 
-    while Modules_while:
-        Choice = core.core_input()
+        Modules_while = True
 
-        if Choice == "1":
-            Advanced_Windows_Meterpreter()
-            Modules_while = False
+        while Modules_while:
 
-        else:
-            core.Clear()
-            core.Module_Choice()
-            core.Bad_Choice()
-            continue
+            Choice = core.core_input()
+
+            if Choice == "1":
+                Advanced_Windows_Meterpreter()
+                Modules_while = False
+
+            elif Choice == "0":
+                core.Clear()
+                print("Exiting...")
+                break
+
+            else:
+                core.Clear()
+                core.Module_Choice()
+                core.Bad_Choice()
+                continue
+
+    except KeyboardInterrupt:
+        core.Exit_Program()
 
 
 
 #Function to select Windows modules
 def Advanced_Windows_Meterpreter():
 
-    core.Windows_Arch_Choice()
+    try:
 
-    AWM_while = True
+        core.Clear()
+        core.Windows_Arch_Choice()
 
-    while AWM_while:
-        Choice = core.core_input()
+        AWM_while = True
 
-        if Choice == "1":
-            AWM_x86()
-            AWM_while = False
+        while AWM_while:
 
-        elif Choice == "2":
-            AWM_x64()
-            AWM_while = False
+            Choice = core.core_input()
 
-        else:
-            core.Clear()
-            core.Windows_Arch_Choice()
-            core.Bad_Choice()
-            continue
+            if Choice == "1":
+                AWM_x86()
+                AWM_while = False
+
+            elif Choice == "2":
+                AWM_x64()
+                AWM_while = False
+
+            elif Choice == "0":
+                Modules()
+                AWM_while = False
+
+            else:
+                core.Clear()
+                core.Windows_Arch_Choice()
+                core.Bad_Choice()
+                continue
+
+    except KeyboardInterrupt:
+        core.Exit_Program()
 
 
 
 #Function to select x86 Windows payload
 def AWM_x86():
 
-    core.x86_Windows_Stager_Choice()
+    try:
 
-    AWM_x86_while = True
+        core.Clear()
+        core.x86_Windows_Stager_Choice()
 
-    while AWM_x86_while:
-        Choice = core.core_input()
+        AWM_x86_while = True
 
-        if Choice == "1":
-            x86_Win_Meterpreter_Rev_Tcp.Construction()
-            AWM_x86_while = False
+        while AWM_x86_while:
 
-        elif Choice == "2":
-            x86_Win_Meterpreter_Rev_Http.Construction()
-            AWM_x86_while = False
+            Choice = core.core_input()
 
-        elif Choice == "3":
-            x86_Win_Meterpreter_Rev_Https.Construction()
-            AWM_x86_while = False
+            if Choice == "1":
+                x86_Win_Meterpreter_Rev_Tcp.Construction()
+                AWM_x86_while = False
 
-        else:
-            core.Clear()
-            core.x86_Windows_Stager_Choice()
-            core.Bad_Choice()
-            continue
+            elif Choice == "2":
+                x86_Win_Meterpreter_Rev_Http.Construction()
+                AWM_x86_while = False
+
+            elif Choice == "3":
+                x86_Win_Meterpreter_Rev_Https.Construction()
+                AWM_x86_while = False
+
+            elif Choice == "0":
+                Advanced_Windows_Meterpreter()
+                AWM_x86_while = False
+
+            else:
+                core.Clear()
+                core.x86_Windows_Stager_Choice()
+                core.Bad_Choice()
+                continue
+
+    except KeyboardInterrupt:
+        core.Exit_Program()
 
 
 
 #Function to select x64 Windows payload
 def AWM_x64():
 
-    core.x64_Windows_Stager_Choice()
+    try:
 
-    AWM_x64_while = True
+        core.Clear()
+        core.x64_Windows_Stager_Choice()
 
-    while AWM_x64_while:
-        Choice = core.core_input()
+        AWM_x64_while = True
 
-        if Choice == "1":
-            x64_Win_Meterpreter_Rev_Tcp.Construction()
-            AWM_x64_while = False
+        while AWM_x64_while:
 
-        elif Choice == "2":
-            x64_Win_Meterpreter_Rev_Http.Construction()
-            AWM_x64_while = False
+            Choice = core.core_input()
 
-        elif Choice == "3":
-            x64_Win_Meterpreter_Rev_Https.Construction()
-            AWM_x64_while = False
+            if Choice == "1":
+                x64_Win_Meterpreter_Rev_Tcp.Construction()
+                AWM_x64_while = False
 
-        else:
-            core.Clear()
-            core.x64_Windows_Stager_Choice()
-            core.Bad_Choice()
-            continue
+            elif Choice == "2":
+                x64_Win_Meterpreter_Rev_Http.Construction()
+                AWM_x64_while = False
 
+            elif Choice == "3":
+                x64_Win_Meterpreter_Rev_Https.Construction()
+                AWM_x64_while = False
+
+            elif Choice == "0":
+                Advanced_Windows_Meterpreter()
+                AWM_x64_while = False
+
+            else:
+                core.Clear()
+                core.x64_Windows_Stager_Choice()
+                core.Bad_Choice()
+                continue
+
+    except KeyboardInterrupt:
+        core.Exit_Program()
 
 
 #Exec modules function
