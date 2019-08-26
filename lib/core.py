0c77af63163a9044eccc65101d644cc20d6587f6
G1ft3dC0d3 <52042546+G1ft3dC0d3@users.noreply.github.com>
2019-08-26 04:47:25 +0200

    Add files via upload

--- lib/core.py
+++ lib/core.py
@@ -14,7 +14,7 @@ def Banner():
             
         This program will be used as part of a security audit
     
-                          Version : 0.1
+                         Version : 1.0.0
                           
                           
     """)
@@ -38,26 +38,35 @@ def core_input():
 
 
 def core_input_pass():
-    Input = input("Press any key to continue : ")
+    Input = input("Press any key to continue.")
     return Input
+
+
+
+def Exit_Program():
+    Clear()
+    print("Exiting...")
+    os.system("exit")
 
 
 
 def Module_Choice():
     print("""
- |-----------------------------------|
- | [1] Advanced Windows Meterpreter; |
- |-----------------------------------|
+ |----------------------|
+ | [1] Windows Stagers; |
+ | [0] Exit Software;   |
+ |----------------------|
     """)
 
 
 
 def Windows_Arch_Choice():
     print("""
- |---------------|
- | [1] Arch x86; | 
- | [2] Arch x64; |
- |---------------|
+ |------------------|
+ | [1] x86 Stagers; | 
+ | [2] x64 Stagers; |
+ | [0] Back;        |
+ |------------------|
     """)
 
 
@@ -68,6 +77,7 @@ def x86_Windows_Stager_Choice():
  | [1] Windows x86 Meterpreter Reverse TCP   (C); |
  | [2] Windows x86 Meterpreter Reverse HTTP  (C); |
  | [3] Windows x86 Meterpreter Reverse HTTPS (C); |
+ | [0] Back;                                      |
  |------------------------------------------------|      
     """)
 
@@ -79,6 +89,7 @@ def x64_Windows_Stager_Choice():
  | [1] Windows x64 Meterpreter Reverse TCP   (C); |
  | [2] Windows x64 Meterpreter Reverse HTTP  (C); |
  | [3] Windows x64 Meterpreter Reverse HTTPS (C); |
+ | [0] Back;                                      |
  |------------------------------------------------| 
     """)
 
