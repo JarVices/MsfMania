ca6f0b6fa3d76057df579ace2de429ff98c1bef4
G1ft3dC0d3 <52042546+G1ft3dC0d3@users.noreply.github.com>
2020-04-14 10:33:53 +0200

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,31 +1,45 @@
 import subprocess
+import time
 from lib import core
 
 def main():
 	
-	print(core.amcolors.OCRA + core.amcolors.BOLD + " [*] Updating system." + core.amcolors.ENDC)
+	print(core.MMCOLORS.OCRA + core.MMCOLORS.BOLD + "[*] Updating system." + core.MMCOLORS.ENDC)
 	subprocess.run(["apt", "update"])
 	subprocess.run(["apt", "-y", "full-upgrade"])
 	subprocess.run(["apt", "-y", "autoremove"])
 	subprocess.run(["apt", "update"])
 	subprocess.run(["apt", "-y", "full-upgrade"])
 	subprocess.run(["apt", "-y", "autoremove"])
-	core.Clear()
-	print(core.amcolors.GREEN + core.amcolors.BOLD + " [+] System updated." + core.amcolors.ENDC)
+	core.CLEAR()
+	print(core.MMCOLORS.GREEN + core.MMCOLORS.BOLD + "[+] System updated." + core.MMCOLORS.ENDC)
 
+	time.sleep(5)
 
+	print(core.MMCOLORS.OCRA + core.MMCOLORS.BOLD + "[*] Installing Gnome-terminal." + core.MMCOLORS.ENDC)
+	subprocess.run(["apt", "-y", "install", "gnome-terminal"])
+	core.CLEAR()
+	print(core.MMCOLORS.GREEN + core.MMCOLORS.BOLD + "[+] Gnome-terminal installed." + core.MMCOLORS.ENDC)
 
-	print(core.amcolors.OCRA + core.amcolors.BOLD + " [*] Installing MinGW." + core.amcolors.ENDC)
+	time.sleep(5)
+
+	print(core.MMCOLORS.OCRA + core.MMCOLORS.BOLD + "[*] Installing MinGW." + core.MMCOLORS.ENDC)
 	subprocess.run(["apt", "-y", "install", "mingw-w64*"])
-	core.Clear()
-	print(core.amcolors.GREEN + core.amcolors.BOLD + " [+] MinGW installed." + core.amcolors.ENDC)	
-
-	
-
-	print(core.amcolors.OCRA + core.amcolors.BOLD + " [*] Making GCC WINDRES link." + core.amcolors.ENDC)	
-	subprocess.run(["ln", "-s", "/usr/bin/x86_64-w64-mingw32-gcc-windres", "/usr/bin/windres"])
+	core.CLEAR()
+	print(core.MMCOLORS.GREEN + core.MMCOLORS.BOLD + "[+] MinGW installed." + core.MMCOLORS.ENDC)
+	print(core.MMCOLORS.OCRA + core.MMCOLORS.BOLD + "[*] Making GCC WINDRES link." + core.MMCOLORS.ENDC)
+	subprocess.run(["ln", "-s", "/usr/bin/x86_64-w64-mingw32-windres", "/usr/bin/windres"])
 	subprocess.run(["ln", "-s", "/usr/bin/i686-w64-mingw32-windres", "/usr/bin/windres_1"])
-	print(core.amcolors.GREEN + core.amcolors.BOLD + " [+] GCC WINDRES link created." + core.amcolors.ENDC)
+	print(core.MMCOLORS.GREEN + core.MMCOLORS.BOLD + "[+] GCC WINDRES link created." + core.MMCOLORS.ENDC)
+
+	time.sleep(5)
+
+	print(core.MMCOLORS.OCRA + core.MMCOLORS.BOLD + "[*] Installing RAR." + core.MMCOLORS.ENDC)
+	subprocess.run(["apt", "-y", "install", "rar"])
+	core.CLEAR()
+	print(core.MMCOLORS.GREEN + core.MMCOLORS.BOLD + "[+] RAR installed.\n" + core.MMCOLORS.ENDC)
+
+	print(print(core.MMCOLORS.GREEN + core.MMCOLORS.BOLD + "[+] MsfMania is ready :) ." + core.MMCOLORS.ENDC))
 
 main()
 
